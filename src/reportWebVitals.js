d7daf4dcd1d249334d190b3e43c6d53333f6e529
David Scherrey III <111524381+ScherreyDavid@users.noreply.github.com>
2023-03-19 19:38:41 -0800

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,4 @@
-#reportWebVitals.js
+//reportWebVitals.js
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
